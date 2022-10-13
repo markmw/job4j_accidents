10bc096c6b892c37323aa9bd0f60795059bda2bf
markmw <dant1kkz@gmail.com>
2022-10-13 12:07:00 +0600

    1. Spring ORM [#2093 #350971]

--- src/main/java/ru/job4j/accidents/persistence/AccidentHibernate.java
+++ src/main/java/ru/job4j/accidents/persistence/AccidentHibernate.java
@@ -1,51 +1,48 @@
 package ru.job4j.accidents.persistence;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class AccidentHibernate {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public Accident save(Accident accident) {
-        try (Session session = sf.openSession()) {
-            session.save(accident);
-            return accident;
-        }
+        crudRepository.run(session -> session.save(accident));
+        return accident;
     }
 
     public List<Accident> getAccidents() {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("from Accident", Accident.class).list();
-        }
+        return crudRepository.query("from Accident", Accident.class);
     }
 
     public Optional<Accident> get(int id) {
-        try (Session session = sf.openSession()) {
-            return session.createQuery(
-                    "from Accident where id = fId", Accident.class)
-                    .setParameter("fId", id)
-                    .uniqueResultOptional();
-        }
+            return crudRepository.optional(
+                    "from Accident where id = fId",
+                    Accident.class,
+                    Map.of("fId", id));
     }
 
     public void update(Accident accident) {
-        try (Session session = sf.openSession()) {
-            session.update(accident);
-        }
+        crudRepository.run(session -> session.createQuery(
+                "update Accident set name = fName, text = fText, address = fAddress, accident_type_id = fAccident_type_id, rules = fRules")
+                .setParameter("fName", accident.getName())
+                .setParameter("fText", accident.getText())
+                .setParameter("fAddress", accident.getAddress())
+                .setParameter("fAccident_type_id", accident.getType())
+                .setParameter("fRules", accident.getRules())
+                .executeUpdate());
     }
 
-    public boolean delete(int id) {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("delete from Accident where id = fId", Accident.class)
-                    .setParameter("fId", id).executeUpdate() > 0;
-        }
+    public void delete(int id) {
+        crudRepository.run(
+                "delete from Accident where id = fId",
+                        Map.of("fId", id));
     }
 }
