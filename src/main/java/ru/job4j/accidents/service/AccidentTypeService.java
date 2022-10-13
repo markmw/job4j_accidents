29efb3bbff2f9c95b65f6b0185df835c0be00a34
markmw <dant1kkz@gmail.com>
2022-10-13 12:18:31 +0600

    2. Spring Data [#296073 #350977]

--- src/main/java/ru/job4j/accidents/service/AccidentTypeService.java
+++ src/main/java/ru/job4j/accidents/service/AccidentTypeService.java
@@ -2,32 +2,33 @@ package ru.job4j.accidents.service;
 
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.AccidentType;
-import ru.job4j.accidents.persistence.AccidentTypeMem;
+import ru.job4j.accidents.persistence.AccidentTypeRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
 @Service
 public class AccidentTypeService {
-    private final AccidentTypeMem accidentTypeMem;
+    private final AccidentTypeRepository accidentTypeRepository;
 
-    public AccidentTypeService(AccidentTypeMem accidentTypeMem) {
-        this.accidentTypeMem = accidentTypeMem;
+    public AccidentTypeService(AccidentTypeRepository accidentTypeRepository) {
+        this.accidentTypeRepository = accidentTypeRepository;
     }
 
     public Optional<AccidentType> get(int id) {
-        return accidentTypeMem.get(id);
+        return accidentTypeRepository.findById(id);
     }
 
     public AccidentType put(AccidentType type) {
-        return accidentTypeMem.put(type);
+        return accidentTypeRepository.save(type);
     }
 
     public Collection<AccidentType> getTypes() {
-        return accidentTypeMem.getTypes();
-    }
-
-    public boolean update(AccidentType type) {
-        return accidentTypeMem.update(type);
+        var rsl = new ArrayList<AccidentType>();
+        for (var accidentType : accidentTypeRepository.findAll()) {
+            rsl.add(accidentType);
+        }
+        return rsl;
     }
 }
--- src/main/java/ru/job4j/accidents/service/RuleService.java
+++ src/main/java/ru/job4j/accidents/service/RuleService.java
@@ -1,46 +1,34 @@
 package ru.job4j.accidents.service;
 
 import org.springframework.stereotype.Service;
-import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.Rule;
-import ru.job4j.accidents.persistence.RuleMem;
+import ru.job4j.accidents.persistence.RuleRepository;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class RuleService {
-    private final RuleMem ruleMem;
+    private final RuleRepository ruleRepository;
 
-    public RuleService(RuleMem ruleMem) {
-        this.ruleMem = ruleMem;
+    public RuleService(RuleRepository ruleRepository) {
+        this.ruleRepository = ruleRepository;
     }
 
     public Optional<Rule> get(int id) {
-        return ruleMem.get(id);
+        return ruleRepository.findById(id);
     }
 
     public Rule put(Rule rule) {
-        return ruleMem.put(rule);
+        return ruleRepository.save(rule);
     }
 
     public Collection<Rule> getRules() {
-        return ruleMem.getRules();
-    }
-
-    public boolean update(Rule rule) {
-        return ruleMem.update(rule);
-    }
-
-    public void setRules(Accident accident) {
-        if (accident != null && !accident.getRules().isEmpty()) {
-            final List<Integer> ids = new ArrayList<>();
-            for (Rule rule : accident.getRules()) {
-                ids.add(rule.getId());
-            }
-            accident.setRules(ruleMem.getByIds(ids));
+        var rsl = new ArrayList<Rule>();
+        for (var rule : ruleRepository.findAll()) {
+            rsl.add(rule);
         }
+        return rsl;
     }
 }
