152060089192f26adb8648c3c22cb10f585e639b
markmw <dant1kkz@gmail.com>
2022-10-14 12:52:57 +0600

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,20 +1,21 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accidents.persistence.AccidentJdbcTemplate;
+import ru.job4j.accidents.persistence.AccidentHibernate;
 
 @Controller
 @AllArgsConstructor
 public class IndexController {
-    private AccidentJdbcTemplate service;
+    private final AccidentHibernate accidentHibernate;
 
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("user", "Petr Arsentev");
-        model.addAttribute("accidents", service.getAll());
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("accidents", accidentHibernate.getAccidents());
         return "index";
     }
 }
